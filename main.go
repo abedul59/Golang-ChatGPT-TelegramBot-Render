4569618f50785e8cfe6ca76abda71d084e20e01b
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-01-04 18:12:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+
+	"fmt"
 	"log"
 	"os"
 
+
+
 	"github.com/yanzay/tbot/v2"
 )
+
 
 func main() {
 	bot := tbot.New(os.Getenv("TELEGRAM_BOT_TOKEN"),
@@ -13,6 +18,9 @@ func main() {
 	c := bot.Client()
 	bot.HandleMessage("ping", func(m *tbot.Message) {
 		c.SendMessage(m.Chat.ID, "pong")
+	})
+	bot.HandleMessage(".*yo.*", func(m *tbot.Message) {
+		c.SendMessage(m.Chat.ID, "hello!hello!hello!")
 	})
 	log.Fatal(bot.Start())
 }
