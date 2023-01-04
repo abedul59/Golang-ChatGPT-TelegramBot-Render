8f1a55449a9b2050d399941e5600b835cc48175d
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-01-04 18:28:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	bot := tbot.New(os.Getenv("TELEGRAM_BOT_TOKEN"),
 		tbot.WithWebhook("https://chatgpt-telebot.onrender.com", ":8080"))
 	c := bot.Client()
-	bot.HandleMessage("ping", func(m *tbot.Message) {
-		c.SendMessage(m.Chat.ID, "pong")
+	bot.HandleMessage(".*yo.*", func(m *tbot.Message) {
+	c.SendMessage(m.Chat.ID, "hello!")
 	})
 	log.Fatal(bot.Start())
 }
